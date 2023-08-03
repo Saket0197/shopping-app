8a5bab9883c9d52c1e6d8fb85efe548dfc505e8e
Saket <saket.s9713@gmail.com>
2023-08-03 16:16:52 +0530

    removed empty cart functionality

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { emptyCart } from '../redux/slices/CartSlice';
-import { useDispatch } from 'react-redux';
+
 
 export default function CheckoutPage() {
-  const dispatch = useDispatch();
+  
   return (
     <div className='flex flex-col gap-4 justify-center items-center w-full h-[100vh]'>
       <p className='sm:text-3xl text-4xl text-[#299158] text-center'>Congratulations! Payment Successful</p>
       <NavLink to='/'>
-        <button className='sm:text-xl w-[70vw] text-2xl font-semibold p-2 rounded-lg border border-slate-500 capitalize bg-[#299158] hover:bg-[#299158b7] transition duration-200 text-[#f3f382]' onClick={() => dispatch(emptyCart())}>Continue Shopping</button>
+        <button className='sm:text-xl w-[70vw] text-2xl font-semibold p-2 rounded-lg border border-slate-500 capitalize bg-[#299158] hover:bg-[#299158b7] transition duration-200 text-[#f3f382]'>Continue Shopping</button>
       </NavLink>
     </div>
   )
